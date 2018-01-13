92c26759a95930fcedfd460e4b88317e7edddb00
Nick Johnson <nicktjohnson93@gmail.com>
2018-01-13 04:20:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var name = "Joe"
 var height = "74"
-height.innerHTML = parseInt(height.innerHTML
+height.innerHTML = parseInt(height.innerHTML) + 4
 
 // Don't worry about this
 module.exports = { name, height
